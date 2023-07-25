925da4412bae886b0d30a12c99de33b1337b791d
Miguel Matey Sanz <al341802@uji.es>
2023-07-25 13:27:35 +0200

    feat(hu01): add note builder function

--- src/app/data/note.ts
+++ src/app/data/note.ts
@@ -1,6 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export interface Note {
   id: string;
   title: string;
   description: string;
   date: Date;
+}
+
+export function createNote(title: string, description: string): Note {
+  return {
+    id: uuidv4(),
+    title,
+    description,
+    date: new Date()
+  }
 }
