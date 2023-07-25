0e017f20195b28d3f6949650d7b111e6653e4f3d
Miguel Matey Sanz <al341802@uji.es>
2023-07-25 13:32:50 +0200

    feat(hu01): implement local repository

--- src/app/data/local/repository.ts
+++ src/app/data/local/repository.ts
@@ -1,22 +1,46 @@
 import { NotesRepository } from "../notes-repository";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 import { Note } from "../note";
+
+const STORAGE_KEY = "notes";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalRepository implements NotesRepository {
 
+  private subject: BehaviorSubject<Array<Note>> = new BehaviorSubject<Array<Note>>([]);
+  private storage = localStorage;
+
   getNoteChanges(): Observable<Array<Note>> {
-    throw new Error("Unimplemented!");
+    const stringified = this.storage.getItem(STORAGE_KEY);
+    let notes: Array<Note>;
+    if (stringified !== null) {
+      notes = JSON.parse(stringified);
+    } else {
+      notes = [];
+    }
+
+    this.subject.next(notes);
+    return this.subject;
   }
 
   insert(note: Note): void {
-    throw new Error("Unimplemented!");
+    const stringified = this.storage.getItem(STORAGE_KEY);
+    let notes: Array<Note>;
+    if (stringified !== null) {
+      notes = JSON.parse(stringified);
+    } else {
+      notes = [];
+    }
+
+    notes.push(note);
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(notes));
+    this.subject.next(notes);
   }
 
   clear(): void {
-    throw new Error("Unimplemented!");
+    this.storage.clear();
   }
 }
