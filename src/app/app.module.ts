22f41de2258935c0db5f0e821e89f0779c311978
Miguel Matey Sanz <al341802@uji.es>
2023-07-25 14:41:45 +0200

    chore: add angular material deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NOTES_REPOSITORY } from "./data/notes-repository";
 import { LocalRepository } from "./data/local/repository";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [{provide: NOTES_REPOSITORY, useClass: LocalRepository}],
   bootstrap: [AppComponent]
