cf9534eb4c009516d292f23542ea00239af1f68c
Miguel Matey Sanz <al341802@uji.es>
2023-07-26 08:53:10 +0200

    feat(hu03): add update implementation

--- src/app/data/local/repository.ts
+++ src/app/data/local/repository.ts
@@ -14,37 +14,40 @@ export class LocalRepository implements NotesRepository {
   private storage = localStorage;
 
   getNoteChanges(): Observable<Array<Note>> {
-    const stringified = this.storage.getItem(STORAGE_KEY);
-    let notes: Array<Note>;
-    if (stringified !== null) {
-      notes = JSON.parse(stringified);
-    } else {
-      notes = [];
-    }
-
+    const notes = this.listNotes();
     this.subject.next(notes);
     return this.subject;
   }
 
   insert(note: Note): void {
-    const stringified = this.storage.getItem(STORAGE_KEY);
-    let notes: Array<Note>;
-    if (stringified !== null) {
-      notes = JSON.parse(stringified);
-    } else {
-      notes = [];
-    }
-
+    const notes = this.listNotes();
     notes.push(note);
     this.storage.setItem(STORAGE_KEY, JSON.stringify(notes));
     this.subject.next(notes);
   }
 
   update(id: string, title: string, description: string): void {
-    throw new Error("Unimplemented!");
+    const notes = this.listNotes();
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    notes[noteIndex] = { ...notes[noteIndex], title, description };
+
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(notes));
+    this.subject.next(notes);
   }
 
   clear(): void {
     this.storage.clear();
+  }
+
+  private listNotes(): Array<Note> {
+    const stringified = this.storage.getItem(STORAGE_KEY);
+    let notes: Array<Note>;
+    if (stringified !== null) {
+      notes = JSON.parse(stringified);
+    } else {
+      notes = [];
+    }
+
+    return notes;
   }
 }
--- src/app/services/notes-controller.service.ts
+++ src/app/services/notes-controller.service.ts
@@ -1,8 +1,9 @@
 import { Inject, Injectable } from '@angular/core';
 import { NOTES_REPOSITORY, NotesRepository } from "../data/notes-repository";
-import { Observable } from "rxjs";
+import { firstValueFrom, Observable } from "rxjs";
 import { createNote, Note } from "../data/note";
 import { EmptyTitleError } from "../errors/empty-title";
+import { NoteNotFoundError } from "../errors/not-found";
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +27,21 @@ export class NotesControllerService {
     this.notesRepository.insert(note);
   }
 
-  async updateNote(id: string, name: string, description: string): Promise<void> {
-    throw new Error("Unimplemented!");
+  async updateNote(id: string, title: string, description: string): Promise<void> {
+    if (title.length === 0) {
+      throw new EmptyTitleError();
+    }
+
+    const idExists = await this.idExists(id);
+    if (!idExists) {
+      throw new NoteNotFoundError(id);
+    }
+
+    this.notesRepository.update(id, title, description);
+  }
+
+  private async idExists(id: string): Promise<boolean> {
+    const notes = await firstValueFrom(this.getNotes());
+    return notes.find((note) => note.id === id) !== undefined;
   }
 }
